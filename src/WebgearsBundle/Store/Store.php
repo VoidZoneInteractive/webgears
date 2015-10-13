fd49955dc5c0024560bd3972457cee3eaf001357
Void Zone <developer+github@voidzone.pl>
2015-10-13 21:08:17 +0200

    - Added missing files

---
+++ src/WebgearsBundle/Store/Store.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: grzegorzgurzeda
+ * Date: 13.10.15
+ * Time: 20:54
+ */
+
+namespace WebgearsBundle\Store;
+
+/**
+ * Class Store - used for storing data into database
+ * @package WebgearsBundle\Store
+ */
+class Store {
+    public function __construct()
+    {
+
+    }
+} 
